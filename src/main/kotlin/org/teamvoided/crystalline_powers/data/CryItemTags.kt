81409d465c44435003e842f50019167a9bb12d45
theendercore <theendercore@gmail.com>
2024-05-27 22:43:35 +0300

    - custom slot

--- src/main/kotlin/org/teamvoided/crystalline_powers/data/CryItemTags.kt
+++ src/main/kotlin/org/teamvoided/crystalline_powers/data/CryItemTags.kt
@@ -3,10 +3,14 @@ package org.teamvoided.crystalline_powers.data
 import net.minecraft.item.Item
 import net.minecraft.registry.RegistryKeys
 import net.minecraft.registry.tag.TagKey
+import net.minecraft.util.Identifier
 import org.teamvoided.crystalline_powers.CrystallinePowers.id
 
 object CryItemTags {
-    val COPPER_TIER = create("copper_tier")
+    val COPPER_SLOT = createSlot("chest/copper_slot")
 
-     fun create(id: String): TagKey<Item> = TagKey.of(RegistryKeys.ITEM, id(id))
+    fun create(id: String): TagKey<Item> = TagKey.of(RegistryKeys.ITEM, id(id))
+
+
+    fun createSlot(id: String): TagKey<Item> = TagKey.of(RegistryKeys.ITEM, Identifier("trinkets", id))
 }
--- src/main/kotlin/org/teamvoided/crystalline_powers/data/gen/ItemTagProvider.kt
+++ src/main/kotlin/org/teamvoided/crystalline_powers/data/gen/ItemTagProvider.kt
@@ -2,12 +2,8 @@ package org.teamvoided.crystalline_powers.data.gen
 
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider
-import net.minecraft.item.Item
 import net.minecraft.item.Items
 import net.minecraft.registry.HolderLookup
-import net.minecraft.registry.RegistryKeys
-import net.minecraft.registry.tag.TagKey
-import net.minecraft.util.Identifier
 import org.teamvoided.crystalline_powers.data.CryItemTags
 import org.teamvoided.crystalline_powers.init.CryItems
 import java.util.concurrent.CompletableFuture
@@ -15,16 +11,9 @@ import java.util.concurrent.CompletableFuture
 class ItemTagProvider(o: FabricDataOutput, c: CompletableFuture<HolderLookup.Provider>) :
     FabricTagProvider.ItemTagProvider(o, c) {
 
-    val CHEST_NECKLACE = itemTag("trinkets","chest/necklace")
-
     override fun configure(provider: HolderLookup.Provider) {
-        getOrCreateTagBuilder(CHEST_NECKLACE)
-            .addTag(CryItemTags.COPPER_TIER)
+        getOrCreateTagBuilder(CryItemTags.COPPER_SLOT)
             .add(Items.AMETHYST_SHARD)
-
-        getOrCreateTagBuilder(CryItemTags.COPPER_TIER)
             .add(CryItems.AMETHYST_PENDANT)
-
     }
-    fun itemTag(path:String,id: String): TagKey<Item> = TagKey.of(RegistryKeys.ITEM, Identifier(path, id))
 }
