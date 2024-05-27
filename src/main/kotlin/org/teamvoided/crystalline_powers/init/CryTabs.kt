f6694ab38e39c6b644d4a6c840e8ebe1ba079dd9
theendercore <theendercore@gmail.com>
2024-05-27 22:16:39 +0300

    - formated tab

--- src/main/kotlin/org/teamvoided/crystalline_powers/init/CryTabs.kt
+++ src/main/kotlin/org/teamvoided/crystalline_powers/init/CryTabs.kt
@@ -8,21 +8,23 @@ import net.minecraft.text.Text
 import org.teamvoided.crystalline_powers.CrystallinePowers.id
 
 object CryTabs {
-    fun init(){}
+    fun init() {}
     val CRYSTALLINE_POWERS_TAB = register(
         "crystaline_powers_tab",
         FabricItemGroup.builder()
             .icon { CryItems.AMETHYST_PENDANT.defaultStack }
             .name(Text.translatable("Crystaline powers"))
-            .entries {_, entries ->
+            .entries { _, entries ->
                 entries.addStacks(
                     mutableSetOf(
-                    CryItems.AMETHYST_PENDANT.defaultStack,
-                    ))
-
+                        CryItems.AMETHYST_PENDANT.defaultStack,
+                    )
+                )
             }
             .build()
     )
+
     fun register(id: String, tab: ItemGroup): ItemGroup {
         return Registry.register(Registries.ITEM_GROUP, id(id), tab)
-}}
+    }
+}
