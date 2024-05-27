507f0d4febbeb47b8c0c3cdf2ec700b956102717
Astratine <davebobsos910@gmail.com>
2024-05-27 19:36:43 +0100

    added one item

--- src/main/kotlin/org/teamvoided/crystalline_powers/init/CryItems.kt
+++ src/main/kotlin/org/teamvoided/crystalline_powers/init/CryItems.kt
@@ -1,5 +1,14 @@
 package org.teamvoided.crystalline_powers.init
 
+import net.minecraft.item.Item
+import net.minecraft.registry.Registries
+import net.minecraft.registry.Registry
+import org.teamvoided.crystalline_powers.CrystallinePowers.id
+
 object CryItems {
     fun init() {}
+    val AMETHYST_PENDANT = register("amethyst_pendant", Item(Item.Settings()))
+    fun register(id: String, item: Item): Item {
+        return Registry.register(Registries.ITEM, id(id), item)
+    }
 }
